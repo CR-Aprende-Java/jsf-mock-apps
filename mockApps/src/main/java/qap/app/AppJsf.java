94c3e3a8861e9b5061d75d3ce7d8a607a4174fc8
sieteideas <ch.roig@gmail.com>
2017-08-07 15:45:12 -0300

    Logos de apps

---
+++ mockApps/src/main/java/qap/app/AppJsf.java
@@ -0,0 +1,61 @@
+package qap.app;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author christian
+ */
+@ManagedBean(name = "app") // creado en faces-config
+@SessionScoped
+public class AppJsf {
+    private AppModel info;
+    
+    //Props App
+    private String appKey = DEFAULT_APP;
+    private String theme  = DEFAULT_THEME;
+    private String color  = DEFAULT_COLOR;
+    private static final String DEFAULT_APP   = "qap";
+    private static final String DEFAULT_THEME = "qap-material";
+    private static final String DEFAULT_COLOR = "original";
+    
+
+    public AppJsf() {
+        //info = appSvc.getInfo("Qap"); ... si hay logica de negocio, sino
+        info = new AppModel("Qap");
+    }
+
+    public AppModel getInfo() {
+        return info;
+    }
+            
+    //Getters y setters
+
+    public String getAppKey() {
+        return appKey;
+    }
+
+    public void setAppKey(String appKey) {
+        this.appKey = appKey;
+        info = new AppModel(appKey);
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    
+}
